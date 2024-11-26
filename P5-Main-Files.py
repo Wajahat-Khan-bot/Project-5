4b3360321872f32f5fd2485ab14055236c41fc6f
Wajahat Khan <wajahatkhan4477@gmail.com>
2024-11-26 11:16:24 +0500

    code updated

--- P5-Main-Files.py
+++ P5-Main-Files.py
@@ -17,6 +17,23 @@ class Account:
         print("Now your balence is", self.balence)
 
 
-a1 = Account(10000, 2313)
-a1.debit(500)
-a1.credit(2000)
+while True:
+    init_deposit = input("Please enter your initial deposit: ")
+
+    if init_deposit.isdigit():
+        init_deposit = int(init_deposit)
+        if init_deposit <= 0:
+            print("Please enter amount more than zero.")
+            continue
+        else:
+            break
+    else:
+        print("Invalid Entry")
+        continue
+
+print("You initial deposit is Rs.", init_deposit)
+
+
+# a1 = Account(10000, 2313)
+# a1.debit(500)
+# a1.credit(2000)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+if init_deposit <= 0:
+            print("Please enter amount more than zero.")
+            continue
+        else:
