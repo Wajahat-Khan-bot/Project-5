129f9c079cd4cc24edc968f3d13d18a3ddb8d093
Wajahat Khan <wajahatkhan4477@gmail.com>
2024-11-26 16:19:47 +0500

    new features

--- P5-Main-Files.py
+++ P5-Main-Files.py
@@ -8,12 +8,12 @@ class Account:
     def debit(self, amount):
         self.balence -= amount
         print("This amount", amount, "is debeted")
-        print("Now your balence is", self.balence)
+        print("Now your balence is Rs.", self.balence)
 
     def credit(self, amount):
         self.balence += amount
         print(amount, "is credited in your account.")
-        print("Now your balence is", self.balence)
+        print("Now your balence is Rs.", self.balence)
 
 
 while True:
@@ -34,8 +34,21 @@ print("You initial deposit is Rs.", init_deposit)
 
 a1 = Account(init_deposit)
 
-ask = input("Do you want to debit or credit amount?\n").lower()
 
-if ask == "debit":
-    amount = input("How much amount do you want to debit?\n")
-    a1.debit(amount)
+while True:
+    ask = input("Do you want to debit or credit amount?\n").lower()
+    if ask == "debit":
+        amount = input("How much amount do you want to debit?\n")
+        if amount.isdigit():
+            amount = int(amount)
+            a1.debit(amount)
+            break
+    elif ask == "credit":
+        amount = input("How much amount do you want to credit?\n")
+        if amount.isdigit():
+            amount = int(amount)
+            a1.credit(amount)
+            break
+    else:
+        print("Invalid Entry")
+        continue
