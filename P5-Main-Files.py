b674b6ece346bab21d3e795888be2b26a7d3e1c4
Wajahat Khan <wajahatkhan4477@gmail.com>
2024-11-27 10:55:22 +0500

    code updated

--- P5-Main-Files.py
+++ P5-Main-Files.py
@@ -10,12 +10,12 @@ class Account:
             print("Insufficiant Funds\n")
         else:
             self.balence -= amount
-            print("This amount", amount, "is debeted")
+            print("This amount Rs.", amount, "is debeted")
             print("Now your balence is Rs.", self.balence, "\n")
 
     def credit(self, amount):
         self.balence += amount
-        print(amount, "is credited in your account.")
+        print("Rs.", amount, "is credited in your account.")
         print("Now your balence is Rs.", self.balence, "\n")
 
 
@@ -33,13 +33,14 @@ while True:
         print("Invalid Entry")
         continue
 
-print("You initial deposit is Rs.", init_deposit,"\n")
+print("You initial deposit is Rs.", init_deposit, "\n")
 
 a1 = Account(init_deposit)
 
 
 while True:
-    ask = input("->Do you want to 'Debit' or 'Credit' amount?\nor write 'done' to leave.\n").lower()
+    ask = input(
+        "->Do you want to 'Debit' or 'Credit' amount?\nor write 'done' to leave.\n").lower()
     if ask == "debit":
         while True:
             amount = input("->How much amount do you want to debit?\n")
